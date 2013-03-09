79c53f05bb448382aed41e628d86d8ee09382a72
George Pearman <georgepearman@georgepearman.com>
2013-03-08 18:30:53 -0800

    Deleted hello world

--- GameModels/Risk/Risk/main.m
+++ GameModels/Risk/Risk/main.m
@@ -13,8 +13,7 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
         
     }
     return 0;
