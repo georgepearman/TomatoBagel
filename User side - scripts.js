c309c51476558b9c544f227ee15edec5f83f3be5
Max Morton <max.blrs@gmail.com>
2013-03-09 06:51:31 -0800

    Added South America and prepared the message for sending.

--- User side - scripts.js
+++ User side - scripts.js
@@ -1,5 +1,6 @@
 //Invariants: Countries, who they are next to, possible cards?
 //var CountryArray = newArray(42);
+var GameID;
 var PlayerID;
 var currentPlayer;
 var canvas;
@@ -11,12 +12,18 @@ var ClickRangeOfCountryDict = [
 	[64,60,84,84,"Alaska"],
 	[106,99,168,129,"Alberta"],
 	[99,221,185,294,"CentralAmerica"],
-	[182,148,202,211,"EasternUnitedStates"],
+	[175,159,221,207,"EasternUnitedStates"],
 	[352,16,436,66,"Greenland"],
 	[127,59,219,84,"NorthwestTerritory"],
 	[199,93,235,138,"Ontario"],
 	[258,97,311,137,"Quebec"],
-	[86,142,141,194,"WesternUnitedStates"]
+	[86,142,141,194,"WesternUnitedStates"],
+	[210,298,305,330,"Venezuela"],
+	[245,447,314,580,"Argentina"],
+	[186,352,234,420,"Peru"],
+	[236,396,281,438,"Peru"],
+	[246,343,346,392,"Brazil"],
+	[299,403,399,465,"Brazil"]
 ];
 //var ColorToCountryDict = {"808000":"Alaska","FFFF00":"Alberta","FFFF80":"CentralAmerica","E3D700":"EasternUnitedStates","F8EF00":"Greenland","505027":"NorthwestTerritory","949449":"Ontario","FFF133":"Quebec","B19C00":"WesternUnitedStates","FF0000":"Argentina","804040":"Brazil","800000":"Peru","FF8080":"Venezuela","004080":"GreatBritain","0000FF":"Iceland","00ABE1":"NorthernEurope","0080FF":"Scandinavia","4778CC":"SouthernEurope","000080":"Ukraine","006EAE":"WesternEurope","AE5700":"Congo","FF8000":'EastAfrica',"804000":"Egypt","F3A94E":"Madagascar","FF915B":"NorthAfrica","7A5841":"SouthAfrica","80FF80":"Afghanistan","008040":"China","008080":"India","81bc43":"Irkutsk","80FF00":"Japan","5D8156":"Kamchatka","008000":"MiddleEast","004000":"Mongolia","50B100":"Siam","008A20":"Siberia","2F5029":"Ural","005B38":"Yakutsk","400040":"EasternAustralia","8000FF":"Indonesia","8800C2":"NewGuinea","800040":"WesternAustralia"};
 var imgsource = 'http://myusername.beryl.feralhosting.com/DL/risk-board.png';
@@ -24,13 +31,14 @@ var imgsource = 'http://myusername.beryl.feralhosting.com/DL/risk-board.png';
 
 function decodeMessage(message)
 {
+	GameID = 252;
 	PlayerID = 1;
 	currentPlayer = 3;
 }
 
 function encodeMessage(message)
 {
-
+	return GameID+"@"+PlayerID+"@"+message;
 }
 
 function receiveMessage(message)
@@ -114,7 +122,7 @@ function composeMessage()
 	{
 		if(determineAction(X,Y) != false)
 		{
-			composedmessage += determineAction(X,Y) + "@@@";	
+			composedmessage += determineAction(X,Y) + "@";	
 			encodeMessage(composedmessage);
 			sendMessage(composedmessage);
 			updateDebug(X,Y,composedmessage);
@@ -126,7 +134,7 @@ function composeMessage()
 		{
 			composedmessage += convertAreaToCountry(X,Y);
 			//composedmessage += ""+X+ " " + Y + "";
-			composedmessage +="@@@";
+			composedmessage +="@";
 			updateDebug(X,Y,composedmessage);
 			window.requestAnimationFrame(composeMessage);
 		}
