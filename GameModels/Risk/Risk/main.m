34f8cd57a97b37a82af6a9bc34468954d6571abf
George Pearman <georgepearman@georgepearman.com>
2013-03-08 18:25:49 -0800

    Added Risk Game Model Files.
    No data in them yet.

---
+++ GameModels/Risk/Risk/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Risk
+//
+//  Created by George Pearman on 3/8/13.
+//  Copyright (c) 2013 George Pearman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
